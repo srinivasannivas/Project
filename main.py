40f97fbeb23e508a5bc11bfb9e409115084a9940
Srinivasan C <132070116+srinivasannivas@users.noreply.github.com>
2025-04-08 00:32:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -76,6 +76,6 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 # === Run the server ===
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+#if __name__ == "__main__":
+    #port = int(os.environ.get("PORT", 5000))
+    #app.run(host='0.0.0.0', port=port, debug=True)
